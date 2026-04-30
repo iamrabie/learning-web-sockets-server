7b11a67e0bb3fd690cb45f627d08b1cc6ad1f6ed
Rabia Zahid <zahidrabia048@gmail.com>
2026-04-30 16:38:10 +0500

    user connected successfullu using io()

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
 const express = require("express");
 const { createServer } = require("node:http");
+const { join } = require("node:path");
+const { Server } = require("socket.io");
 
 const app = express();
 const server = createServer(app);
+const io = new Server(server);
+
 
 app.get('/' , (req , res) => {
-    res.send("<h2>Hellow World</h2>");
+
+    res.sendFile(join(__dirname , 'index.html'));
+    // res.send("<h2>Hellow World</h2>");
+    // console.log("CURRENT DIRECTORY ::::::" , __dirname); GETS THE CURRENT DIRECTORY
+});
+
+
+//It is a predefined event that Socket.io automatically emits, It fires when a client successfully connects to the server. isolated per client connection,Socket.io creates a separate socket object for each user.
+io.on('connection' , (socket) => {
+    console.log('a user connected');
 });
 
 
 server.listen(8000 , () => {
-    console.log("server running at https://localhost:8000");
+    console.log("server running at http://localhost:8000");
 });

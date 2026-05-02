f65bba9c14e91fca1e9f5885c110fb0e7f7295ef
Rabia Zahid <zahidrabia048@gmail.com>
2026-05-02 19:02:21 +0500

    learned and implemented three emit methods i.e. socket.emit() , socket.broadcast.emit() and io.emit().

--- index.js
+++ index.js
@@ -19,8 +19,25 @@ app.get('/' , (req , res) => {
 //It is a predefined event that Socket.io automatically emits, It fires when a client successfully connects to the server. isolated per client connection,Socket.io creates a separate socket object for each user.
 io.on('connection' , (socket) => {
     console.log('a user connected');
+
+    //   socket.broadcast.emit('hi');
+
+    //   socket.emit('hi');
+
+
+    socket.on('chat message' , (message) => {
+        console.log('MESSAGE' , message);
+        socket.emit('hi');
+    });
+
+
 });
 
+
+
+//socket.emit() --> emit means send from where i am, if used on client side, it sends the message to the server and if used on the server; it sends message to the client side.
+//io.emit() --> sends message to all the clients.
+//socket.broadcast.emit() --> sends message to all the clients except the sender.
 
 server.listen(8000 , () => {
     console.log("server running at http://localhost:8000");

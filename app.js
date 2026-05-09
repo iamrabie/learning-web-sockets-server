fef7dad04a9dc01b8c6d0c8d08b43f3bbc22860a
Rabia Zahid <zahidrabia048@gmail.com>
2026-05-09 16:15:24 +0500

    created a file for socket io task

--- app.js
+++ app.js
@@ -1,32 +1,22 @@
-const express = require("express");
 const { createServer } = require("node:http");
 const { join } = require("node:path");
 const { Server } = require("socket.io");
 
+const express = require("express");
 const app = express();
 const server = createServer(app);
 const io = new Server(server);
 
 
-app.get('/' , (req , res) => {
-    res.sendFile(join(__dirname , "user-input.html"));
-    console.log("directory name ::::::" , __dirname);
+
+app.get('/' , (req , res , next) => {
+    // res.send("<h1>Hellow world!</h1>");
+    res.sendFile(join(__dirname , "task.html" ));
+});
+
+server.listen(8000, () => {
+    console.log("app running on http://localhost:8000");
 });
 
 
-io.on("connection" , (socket) => {
-
-    console.log("user connected");
-
-    socket.join("room1");
-
-    console.log("room joined" , socket.rooms);
-
-    socket.on('send-message' , (text) => {
-        console.log("message recieved ::::::::" , text);
-    });
-});
-
-server.listen(8000 , () => {
-    console.log("server running at http://localhost:8000");
-});
+//ERR: cant set headers after they are sent --> when server tries to send response more than once for the same request.res.send(), res.json(), res.end(), res.sendFile();
